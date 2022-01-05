b3392558cfcf651bf966c4d13005129b8b6a1b64
TaliBenedek <67157784+TaliBenedek@users.noreply.github.com>
2022-01-05 14:12:14 -0500

    Added requires to module-info to use java.awt

---
+++ src/main/java/com/example/coloringpage/ImageToColoringPageConverter.java
@@ -0,0 +1,11 @@
+package com.example.coloringpage;
+
+import java.awt.image.BufferedImage;
+
+public class ImageToColoringPageConverter
+{
+    public BufferedImage toGrayscaleImage()
+    {
+        return null;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.coloringpage {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.coloringpage to javafx.fxml;
