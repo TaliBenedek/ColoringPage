615d17ae3dee7f138fe26f0398228af388e7b30b
TaliBenedek <67157784+TaliBenedek@users.noreply.github.com>
2022-01-05 14:29:03 -0500

    Added grayscale method

--- src/main/java/com/example/coloringpage/ImageToColoringPageConverter.java
+++ src/main/java/com/example/coloringpage/ImageToColoringPageConverter.java
@@ -1,11 +1,27 @@
 package com.example.coloringpage;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class ImageToColoringPageConverter
 {
-    public BufferedImage toGrayscaleImage()
+    public BufferedImage toGrayscaleImage(BufferedImage image)
     {
-        return null;
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for(int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                Color originalColor = new Color(image.getRGB(j, i));
+                int red = (int)(originalColor.getRed() * 0.299);
+                int green = (int)(originalColor.getGreen() * 0.587);
+                int blue = (int)(originalColor.getBlue() * 0.114);
+                int gray = red + green + blue;
+                Color newColor = new Color(gray, gray, gray);
+                image.setRGB(j, i, newColor.getRGB());
+            }
+        }
+        return image;
     }
 }
