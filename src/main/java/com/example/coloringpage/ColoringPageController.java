30801f91e2c50878b2744a1a8af109e46a2713be
TaliBenedek <67157784+TaliBenedek@users.noreply.github.com>
2022-01-08 21:31:43 -0500

    Renamed gui files

--- src/main/java/com/example/coloringpage/ColoringPageController.java
+++ src/main/java/com/example/coloringpage/ColoringPageController.java
@@ -3,7 +3,7 @@ package com.example.coloringpage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController
+public class ColoringPageController
 {
     @FXML
     private Label welcomeText;
