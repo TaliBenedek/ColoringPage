7833568fbff80582b9d6958be28a248fe77b6c1c
TaliBenedek <67157784+TaliBenedek@users.noreply.github.com>
2022-01-11 01:19:43 -0500

    Added more tests

--- src/test/java/ColoringPageControllerTest.java
+++ src/test/java/ColoringPageControllerTest.java
@@ -1,48 +1,61 @@
 import com.example.coloringpage.ColoringPageController;
 import com.example.coloringpage.ImageToColoringPageConverter;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.mockito.Mockito.*;
 
 class ColoringPageControllerTest
 {
     @Test
-    void onUploadButtonClick_validFilePath()
+    void onBrowseButtonClick()
     {
         //given
-        TextField fileNameTextField = mock(TextField.class);
-        doReturn("KittenBackground.jpg").when(fileNameTextField).getText();
-
         ImageView originalImageView = mock(ImageView.class);
-        ImageView modifiedImageView = mock(ImageView.class);
-
+        TextField fileNameTextField = mock(TextField.class);
         ImageToColoringPageConverter converter = new ImageToColoringPageConverter();
-
-        ColoringPageController controller = new ColoringPageController(converter);
+        File file = mock(File.class);
+        ColoringPageController controller = new ColoringPageController(converter, file);
+        doReturn(mock(File.class)).when(file).getAbsoluteFile();
 
         //when
-        controller.onUploadButtonClick();
+        controller.onBrowseButtonClick();
 
         //then
+        verify(fileNameTextField).setText(file.getAbsolutePath());
         verify(originalImageView).setImage(any(Image.class));
+    }
+
+    @Test
+    void onConvertButtonClick()
+    {
+        //given
+        ImageView modifiedImageView = mock(ImageView.class);
+        ImageToColoringPageConverter converter = new ImageToColoringPageConverter();
+        ColoringPageController controller = new ColoringPageController();
+
+        //when
+        controller.onConvertButtonClick(mock(ActionEvent.class));
+
+        //then
         verify(modifiedImageView).setImage(any(Image.class));
     }
 
     @Test
-    void onUploadButtonClick_invalidFilePath()
+    void onSaveButtonClick()
     {
         //given
         ImageToColoringPageConverter converter = new ImageToColoringPageConverter();
-        ColoringPageController controller = new ColoringPageController(converter);
-        TextField fileNameTextField = mock(TextField.class);
-        ImageView originalImageView = mock(ImageView.class);
-        ImageView modifiedImageView = mock(ImageView.class);
+        File file = mock(File.class);
+        ColoringPageController controller = new ColoringPageController(converter, file);
 
         //when
-        controller.onUploadButtonClick();
+        controller.onSaveButtonClick(mock(ActionEvent.class));
 
         //then
     }
