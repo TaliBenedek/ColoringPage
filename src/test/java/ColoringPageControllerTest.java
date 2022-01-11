82f44a0c0b14d05e128f08e62893498e4911c0f8
TaliBenedek <67157784+TaliBenedek@users.noreply.github.com>
2022-01-10 20:43:05 -0500

    Added mockito dependency and switched tests to test module

--- src/test/java/ColoringPageControllerTest.java
+++ src/test/java/ColoringPageControllerTest.java
@@ -1,14 +1,11 @@
-package com.example.coloringpage;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ColoringPageControllerTest
 {
     @BeforeAll
-    public static void beforeClass() {
+    public static void beforeClass()
+    {
         com.sun.javafx.application.PlatformImpl.startup(()->{});
     }
 
---
+++ src/test/java/ImageToColoringPageConverterTest.java
@@ -0,0 +1,21 @@
+import com.example.coloringpage.ImageToColoringPageConverter;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import static org.mockito.Mockito.*;
+
+class ImageToColoringPageConverterTest
+{
+    @Test
+    void getColoringPage()
+    {
+        //given
+        ImageToColoringPageConverter converter = new ImageToColoringPageConverter();
+        File file = mock(File.class);
+
+        //when
+        
+
+        //then
+    }
+}
